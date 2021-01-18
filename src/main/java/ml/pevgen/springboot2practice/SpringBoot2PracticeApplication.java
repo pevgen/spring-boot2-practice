0887e8cd97fd6b2f3e3c7a6e088d70f63b69515f
Evgeny Polyak <epolyak@luxoft.com>
2021-01-18 09:53:15 +0300

    init

---
+++ src/main/java/ml/pevgen/springboot2practice/SpringBoot2PracticeApplication.java
@@ -0,0 +1,13 @@
+package ml.pevgen.springboot2practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2PracticeApplication.class, args);
+    }
+
+}
